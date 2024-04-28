3babb49976f644ea415dc7b6b8f640ca60bdecbd
IraPavlikha <148654091+IraPavlikha@users.noreply.github.com>
2024-04-28 12:47:55 +0300

    Add files via upload

---
+++ client/client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Client
+{
+    static void Main()
+    {
+        IPAddress serverIP = IPAddress.Parse("127.0.0.1");
+        int serverPort = 12345;
+        TcpClient client = new TcpClient();
+        client.Connect(serverIP, serverPort);
+        Console.WriteLine("Виберіть 'date' або 'time':");
+        string request = Console.ReadLine();
+        NetworkStream stream = client.GetStream();
+        byte[] data = Encoding.UTF8.GetBytes(request);
+        stream.Write(data, 0, data.Length);
+        byte[] responseData = new byte[256];
+        int bytes = stream.Read(responseData, 0, responseData.Length);
+        string response = Encoding.UTF8.GetString(responseData, 0, bytes);
+        Console.WriteLine("Отримано від сервера: " + response);
+        client.Close();
+    }
+}
